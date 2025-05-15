83b712e85ca850e9d10f528e201d5e5164f259b9
rajniexpinator <55794136+rajniexpinator@users.noreply.github.com>
2025-05-15 12:47:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = require("./middlewares/uploadMiddleware.js");
+const {
+  uploadBase64ToS3Bucket,
+  uploadToS3Bucket,
+} = require("./config/S3ImageUpload.js");
 const port = process.env.PORT || 5050;
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -19,8 +25,8 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 //   });
 // });
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "550mb" }));
+app.use(bodyParser.urlencoded({ limit: "505mb", extended: true }));
 
 app.use(cors({ origin: "*" }));
 
@@ -31,6 +37,18 @@ app.get("/", (req, res) => {
 app.use("/api/v1", require("./routes/index"));
 
 // Start the server
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  // console.log(req.file);
+
+  if (!req.file) return res.status(400).json({ error: "No file uploaded" });
+  let r = await uploadToS3Bucket(req.file);
+  console.log(r);
+
+  const url = `http://localhost:${5050}/uploads/${req.file.filename}`;
+  res.json({ message: "Upload successful", url, r });
+});
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
